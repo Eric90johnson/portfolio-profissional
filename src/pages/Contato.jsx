d3d2a6e4c50bb1397b0af1d63bd3c438eac959a3
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2026-01-14 00:21:20 -0300

    Criação do arquivo Contato

---
+++ src/pages/Contato.jsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+export function Contato() {
+  return (
+    <div className="content-area">
+      <h2>Fale Comigo</h2>
+      <p>Entre em contato para parcerias ou oportunidades.</p>
+      {/* Depois colocaremos um formulário aqui */}
+    </div>
+  )
+}
