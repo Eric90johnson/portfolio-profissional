2ee5b2cf68adf8d9e6e6a566546b006e5d9366cc
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2026-01-14 00:24:43 -0300

    atualização das rotas em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Sidebar } from './components/Sidebar';
-import { Projects } from './components/Projects'; // <--- Importação com chaves {}
+import { Home } from './pages/Home';
+import { Sobre } from './pages/Sobre';
+import { Contato } from './pages/Contato';
 import './App.css';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Sidebar />
-      <main>
-        <Projects />
-      </main>
-    </div>
+    <HashRouter>
+      <div className="wrapper">
+        
+        {/* A Sidebar fica FORA das rotas, pois ela aparece em todas as páginas */}
+        <Sidebar />
+        
+        <main>
+          {/* Aqui é a área dinâmica que muda conforme o link */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/contato" element={<Contato />} />
+          </Routes>
+        </main>
+        
+      </div>
+    </HashRouter>
   )
 }
 
