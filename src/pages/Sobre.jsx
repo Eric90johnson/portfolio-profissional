eef1ac1378ee8dae83f8e4240d9f7fd9da4b8a94
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2026-01-14 00:19:12 -0300

    criação do arquivo Sobre

---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,13 @@
+import '../App.css'; // Para aproveitar os estilos globais
+
+export function Sobre() {
+  return (
+    <div className="content-area">
+      <h2>Sobre Mim</h2>
+      <p>Olá! Sou o Eric Johnson, um desenvolvedor apaixonado por tecnologia.</p>
+      <br />
+      <p>Minha jornada começou com...</p>
+      {/* Depois preenchemos com sua história real */}
+    </div>
+  )
+}
